e2867c5d3a89af43aaffc5678af221f9e8cde5bf
陈琦 <aseaday@hotmail.com>
2014-06-01 02:33:57 +0800

    fuck

--- brain/main.py
+++ brain/main.py
@@ -1,2 +1,23 @@
-import tornado
-import redis
+# coding = utf-8
+#! /usr/bin/python3
+import tornado.web
+import string, os
+from ser import serers
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        global handler
+        self.write(str(handler.get()))
+
+
+if __name__ == "__main__":
+  settings = {
+  "debug": True,
+  }
+  handler = serers()
+  application = tornado.web.Application([
+      (r"/get", MainHandler),
+    ],**settings)
+  application.listen(80)
+  tornado.ioloop.IOLoop.instance().start()
--- brain/ser.py
+++ brain/ser.py
@@ -1,20 +1,18 @@
-#!/usr/bin/env python2.7
+#! /usr/bin/env python2.7
 import sys, os, serial, threading
 from string import strip
+from time import time
 try:
-    from serial.tools.list_ports import comports
+    from serial.tools.list_ports import ports
 except ImportError:
     comports = None
 
-DEFAULT_PORT = None
-DEFAULT_BAUDRATE = 9600
-DEFAULT_RTS = None
-DEFAULT_DTR = None
+cache = []
 
 
 class serers(object):
 
-    def __init__(self, port, baudrate):
+    def __init__(self,port='/dev/tty.submodem1411', baudrate=115200):
         try:
             self.ser = serial.Serial()
             self.ser.port = port
@@ -22,38 +20,40 @@ class serers(object):
             if not self.ser.isOpen():
                 self.ser.open()
             self.start()
-        except serial.SerialException:
+        except Exception:
             raise
 
     def start(self):
+        global cahce
         self.alive = True
         try:
             self._start_reader()
-            #self._start_writer()
         except threading.ThreadError:
             raise
 
     def _start_reader(self):
-        self._reader_alive = True
-        self.reader_thread = threading.Thread(target=self.reader,)
-        self.reader_thread.setDaemon(False)
-        self.reader_thread.start()
-
-    def _start_writer(self):
-        self._writer_alive = True
-        self._writer_thread = threading.Thread(target=self.writer)
-        self._writer_thread.setDaemon(True)
-        self.reader_thread.start()
-
-    def reader(self):
+        global cache
         try:
-            while self.alive and self._reader_alive:
-                self.text = self.ser.readline()
-        except serial.SerialException:
+            self._reader_alive = True
+            self.reader_thread = threading.Thread(target = self.reader)
+            self.reader_thread.setDaemon(False)
+            self.reader_thread.start()
+        except KeyboardInterrupt:
             raise
 
+    def reader(self):
+        global cache
+        try:
+            while self.alive and self._start_reader:
+                self.text = self.ser.readline()
+                cache.append(str(time())+'#'+self.text)
+        except serial.SerialException, KeyboardInterrupt:
+            raise
 
-if __name__ == '__main__':
-    port = '/dev/tty.usbmodem1411'
-    baudrate = 115200
-    serers(port,baudrate)
+    def get(self):
+        global cache
+        try:
+            return cache.pop()
+        except Exception:
+            return '0'
+
