19f288d5a09cf521e12be43bda4accaaf44d45f9
Hemslo <hemslo.wang@gmail.com>
2014-06-01 11:18:06 +0800

    Use websocket replace http request

--- client/client.py
+++ client/client.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
 
+from ws4py.client import WebSocketBaseClient
+from ws4py.manager import WebSocketManager
+from ws4py import format_addresses, configure_logger
 import pygame
-import time
-import requests
+
+logger = configure_logger()
+
+m = WebSocketManager()
+
+
+class EchoClient(WebSocketBaseClient):
+
+    def __init__(self, url, client):
+        super(EchoClient, self).__init__(url)
+        self.client = client
+
+    def handshake_ok(self):
+        logger.info("Opening %s" % format_addresses(self))
+        m.add(self)
+
+    def received_message(self, msg):
+        logger.info(str(msg))
+        signal = int(str(msg))
+        if signal:
+            self.client.play(signal - 1)
 
 
 class Client(object):
@@ -16,16 +38,22 @@ class Client(object):
     def play(self, index):
         pygame.mixer.find_channel().queue(self.musics[index])
 
-    def fetch(self):
-        r = self.session.get('http://192.168.0.157/get')
-        return int(r.text)
-
     def loop(self):
-        while True:
-            signal = self.fetch()
-            if signal:
-                self.play(signal - 1)
-            time.sleep(0.2)
+        try:
+            m.start()
+            client = EchoClient('ws://192.168.0.157:8090/ws', self)
+            client.connect()
+
+            while True:
+                for ws in m.websockets.itervalues():
+                    if not ws.terminated:
+                        break
+                else:
+                    break
+        except KeyboardInterrupt:
+            m.close_all()
+            m.stop()
+            m.join()
 
 
 def main():
